d449d0933ad1c4ece34f8539cf513679e9a10381
zezeg2 <whdgus003@gmail.com>
2022-07-27 21:09:51 +0900

    first android project

---
+++ app/src/main/java/com/example/sampletestapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sampletestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController : NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavController()
+    }
+}
